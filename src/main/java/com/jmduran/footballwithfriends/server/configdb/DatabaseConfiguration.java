15ecfaf7ddd76f5ef34de720d73b0edf7254fae1
jdurandoming <jdurandoming@csc.com>
2018-05-05 08:47:38 +0200

    fix: fix database config to deploy in Heroku

--- src/main/java/com/jmduran/footballwithfriends/server/configdb/DatabaseConfiguration.java
+++ src/main/java/com/jmduran/footballwithfriends/server/configdb/DatabaseConfiguration.java
@@ -9,7 +9,7 @@ package com.jmduran.footballwithfriends.server.configdb;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -20,16 +20,19 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 @EnableMongoRepositories("com.jmduran.footballwithfriends.server.repositories")
 public class DatabaseConfiguration extends AbstractMongoConfiguration {
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-    @Value("${spring.data.mongodb.port}")
-    private Integer port;
-    @Value("${spring.data.mongodb.username}")
-    private String username;
-    @Value("${spring.data.mongodb.database}")
-    private String database;
-    @Value("${spring.data.mongodb.password}")
-    private String password;
+
+// For local deploy    
+//    @Value("${spring.data.mongodb.host}")
+//    private String host;
+//    @Value("${spring.data.mongodb.port}")
+//    private Integer port;
+//    @Value("${spring.data.mongodb.username}")
+//    private String username;
+//    @Value("${spring.data.mongodb.database}")
+//    private String database;
+//    @Value("${spring.data.mongodb.password}")
+//    private String password;
+    
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
         return new ValidatingMongoEventListener(validator());
@@ -41,13 +44,17 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration {
     
     @Override
     public String getDatabaseName() {
-        return database;
+        // For local deploy
+        // return database;
+        // For Heroku deploy
+        return System.getenv("FWF_DB_DATABASE");
     }
     @Override
     @Bean
     public MongoClient mongoClient() {
         
         // Heroku Config Vars
+        //For Heroku deploy
         String DB_username = System.getenv("FWF_DB_USERNAME");
         String DB_password = System.getenv("FWF_DB_PASSWORD");
         String DB_database = System.getenv("FWF_DB_DATABASE");
@@ -56,11 +63,12 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration {
         // MongoClient mongoClient = new MongoClient(host + ":" + port);
 
         // Mongodb Atlas (Cloud)
+        // For local deploy
         // String uri = "mongodb+srv://" + username + ":" + password + "@fwf-cluster-9z6h8.mongodb.net/" + database;
+        // For Heroku deploy
         String uri = "mongodb+srv://" + DB_username + ":" + DB_password + "@fwf-cluster-9z6h8.mongodb.net/" + DB_database;
         MongoClientURI mongoClientUri = new MongoClientURI(uri);
-        MongoClient mongoClient = new MongoClient(mongoClientUri);
-        
+        MongoClient mongoClient = new MongoClient(mongoClientUri);        
         
         return mongoClient;
     }
