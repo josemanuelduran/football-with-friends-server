e6a02f3ade72cdf9b65be7eafaa36e697208cb1c
Jose Manuel Durán <josemanuelduran@gmail.com>
2022-01-31 13:44:20 +0100

    add a missing import

--- src/main/java/com/jmduran/footballwithfriends/server/controllers/PaymentsController.java
+++ src/main/java/com/jmduran/footballwithfriends/server/controllers/PaymentsController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/fwf/payment")
