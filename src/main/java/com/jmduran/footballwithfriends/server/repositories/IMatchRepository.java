102a19c32032a1b03b528a9251f56c5aa62a9900
Jose Manuel Duran <jdurandoming@dxc.com>
2022-02-06 22:25:51 +0100

    add player stats controller

--- src/main/java/com/jmduran/footballwithfriends/server/repositories/IMatchRepository.java
+++ src/main/java/com/jmduran/footballwithfriends/server/repositories/IMatchRepository.java
@@ -8,7 +8,12 @@ package com.jmduran.footballwithfriends.server.repositories;
 
 import com.jmduran.footballwithfriends.server.models.Match;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface IMatchRepository extends MongoRepository<Match, String> {
     
+    @Query("{ 'callUp.player._id' : ?0, 'cancelled': ?1 }")
+    List<Match> getMatchesPlayedByPlayer(ObjectId playerId, Boolean cancelled);
 }
